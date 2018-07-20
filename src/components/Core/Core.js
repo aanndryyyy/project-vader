a155da33fccfa7d9a3bd0c0eaeadaa73842f8932
Andry Pedak <andry.pedak@gmail.com>
2018-07-20 19:40:50 +0200

    Make landing page view

--- src/components/Core/Core.js
+++ src/components/Core/Core.js
@@ -1,10 +1,19 @@
 import React, {Component, Fragment} from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import asyncComponent from "./../../lib/AsyncComponent";
+
+const Landing = asyncComponent(() => import("./../Landing/Landing"));
 
 export default class Core extends Component {
   render() {
     return (
     <Fragment>
-      <h1>Welcome to Project Vader!</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Landing} />
+        </Switch>
+      </BrowserRouter>
     </Fragment>
     );
   }
