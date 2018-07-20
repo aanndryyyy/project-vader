6e238417d73446c6a9506adf7a3d4831917cf107
Andry Pedak <andry.pedak@gmail.com>
2018-07-20 20:27:17 +0200

    Landing page beginnings

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
 
+import './Landing.css';
+
 export default class Landing extends Component {
   render() {
     return (
-      <h2>Landing page!</h2>
+      <div className="main-wrap">
+        <header>
+          <div className="left">
+            <h1 className="logo">Project Vader</h1>
+          </div>
+          
+          <div className="right">
+            <ul className="nav-list">
+              <li><a href="#">Login</a></li>
+            </ul>
+          </div>
+        </header>
+      </div>
     );
   };
 }
